8ef6dcf72c7ddb1dde6f57c33af6bf58853713b8
Smqkmj <gotenks.ssj499@gmail.com>
2022-01-02 17:30:37 -0500

    finished 00-greetFriend

--- 00-greetFriend/greetFriend.js
+++ 00-greetFriend/greetFriend.js
@@ -1,6 +1,7 @@
 function greetFriend(name) {
   // Insert code here;
+  var greeting = ("Hello, " + name + "!");
+  return greeting;
 }
-
 // Do not edit this line;
-module.exports = greetFriend;
+module.exports = greetFriend; 
